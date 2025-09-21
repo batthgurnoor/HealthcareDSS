7dd8f0bf8ed9550fc0e8d871acc2a5f04efe9d1e
Gurnoor Singh Batth <gurnoorbatth10@gmail.com>
2025-09-20 20:40:07 -0700

    Fixed issue with tailwind not working, turn out it was just a version error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+import forms from "@tailwindcss/forms";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: { card: "0 10px 20px rgba(0,0,0,.25)" }
+    },
   },
-  plugins: [],
-}
-
+  plugins: [forms], 
+};
